e28b8d54b962e743699e32189bd492e4a6ef98f0
Diana <diana34511@gmail.com>
2022-04-15 15:58:45 +0200

    feat: add class Popup

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,6 +1,6 @@
 export default class Popup {
     constructor(popupSelector) {
-        this._popupSelector = popupSelector;
+        this._popupSelector = document.querySelector(popupSelector);
     }
 
     open() {
@@ -10,10 +10,26 @@ export default class Popup {
 
     close() {
         this._popupSelector.classList.remove('popup_opened');
-        document.removeEventListener('keydown', closePopupOnPushEsc);
+        document.removeEventListener('keydown', this._handleEscClose);
     
         setTimeout(() => {
             this._popupSelector.classList.remove('popup__transition');
         }, 500); 
+    }
+
+    _handleEscClose(evt) {
+        if (evt.key === 'Escape') {
+            this.close();
+        }
+    }
+
+    setEventListeners() {
+        this._popupSelector.addEventListener('mousedown', (evt) => {
+            if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+                this.close();
+            }
+        })
+
+        this._popupSelector.addEventListener('keydown', this._handleEscClose);
     }
 }
