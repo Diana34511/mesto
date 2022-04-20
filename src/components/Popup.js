da743d409d2d65741be85ed78450b02e386eaf64
Diana <diana34511@gmail.com>
2022-04-20 19:38:05 +0200

    remove comments

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,7 +5,6 @@ export default class Popup {
 
   open() {
     this._popupSelector.classList.remove("popup_closed");
-    // this._popupSelector.classList.add("popup__transition");
     this._popupSelector.classList.add("popup_opened");
     document.addEventListener("keydown", this._handleEscClose.bind(this));
   }
